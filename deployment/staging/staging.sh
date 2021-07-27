8466598fd1bb1fddc03f04a4c910493df7cde5e0
Wbebey <wilfried@armis.tech>
2021-07-27 12:21:00 +0200

    feat: add ansible and jenkins in the server

--- deployment/staging/staging.sh
+++ deployment/staging/staging.sh
@@ -48,7 +48,6 @@ install_tools() {
 setup_root_login() {
     sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
     # sudo systemctl restart sshd.service
-    # sudo echo "root:rootroot" | chpasswd
 }
 
 setup_welcome_msg() {
