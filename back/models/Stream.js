69500a0589d29cecbd99c36bc6f5bd98607049bf
Alexandre Pinon <alexandre.pinon@epitech.eu>
2021-08-06 18:57:52 +0200

    feat: added Stream model

---
+++ back/models/Stream.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose'
+
+const streamSchema = new mongoose.Schema(
+  {
+    streamer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: 'Streamer is required!',
+      unique: true,
+    },
+    viewers: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: 'User',
+    },
+    type: {
+      type: String,
+      enum: ['GAME', 'IRL'],
+      default: 'GAME',
+    },
+    tags: {
+      type: [String],
+    },
+    game_title: {
+      type: String,
+      default: 'Just Chatting',
+    },
+    title: {
+      type: String,
+      required: 'Title is required!',
+    },
+    description: {
+      type: String,
+    },
+    max_viewers: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export default mongoose.model('Stream', streamSchema)
