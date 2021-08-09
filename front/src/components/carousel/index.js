6b3dc5d3e7b9ae7d783dd3fed25c2deaaf58c909
Alexandre Pinon <alexandre.pinon@epitech.eu>
2021-08-09 14:21:27 +0200

    rebase: resolved conflicts with front

--- front/src/components/carousel/index.js
+++ front/src/components/carousel/index.js
@@ -1,208 +1,199 @@
-import React from "react";
-import Carousel from "react-material-ui-carousel";
-import autoBind from "auto-bind";
-import "../style/Example.scss";
-import { ReactFlvPlayer } from "react-flv-player";
+import React from 'react';
+import Carousel from "react-material-ui-carousel"
+import autoBind from "auto-bind"
+import '../style/Example.scss';
+import { ReactFlvPlayer } from 'react-flv-player'
 
 import {
-  Card,
-  /*     CardContent, */
-  CardMedia,
-  Typography,
-  Grid,
-  /*     Button,
+    Card,
+/*     CardContent, */
+    CardMedia,
+    Typography,
+    Grid,
+/*     Button,
     Checkbox,
     FormControlLabel,
     Radio,
     RadioGroup,
     FormLabel,
     Slider, */
-} from "@material-ui/core";
+} from '@material-ui/core';
 
 function Banner(props) {
-  let items = [];
 
-  const totalItems = props.length ? props.length : 1;
-  const mediaLength = totalItems;
+    let items = [];
 
-  for (let i = 0; i < mediaLength; i++) {
-    const item = props.item.Items[i];
+    const totalItems = props.length ? props.length : 1;
+    const mediaLength = totalItems ;
 
-    const media = (
-      <Grid item xs={12 / totalItems} key={item.Name}>
-        <CardMedia className="Media" image={item.Image} title={item.Name}>
-          {/*                     <ReactFlvPlayer
+    for (let i = 0; i < mediaLength; i++) {
+        const item = props.item.Items[i];
+
+        const media = (
+            <Grid item xs={12 / totalItems} key={item.Name}>
+                <CardMedia
+                    className="Media"
+                    image={item.Image}
+                    title={item.Name}
+                >
+                    <ReactFlvPlayer
                         url="https://www.youtube.com/watch?v=GF04QkRU4es"
                         height="auto"
-                        width="1200px"
+                        width="100%"
                         isMuted={false}
-                    /> */}
-          <Typography hidden className="MediaCaption">
-            {item.Name}
-          </Typography>
-        </CardMedia>
-      </Grid>
-    );
+                    />
+                    <Typography hidden className="MediaCaption">
+                        {item.Name}
+                    </Typography>
+                </CardMedia>
 
-    items.push(media);
-  }
+            </Grid>
+        )
 
-  return (
-    <Card raised className="Banner">
-      <Grid container spacing={0} className="BannerGrid">
-        {items}
-      </Grid>
-    </Card>
-  );
+        items.push(media);
+    }
+
+    return (
+        <Card raised className="Banner">
+            <Grid container spacing={0} className="BannerGrid">
+                {items}
+            </Grid>
+        </Card>
+    )
 }
 
 const items = [
-  {
-    Name: "One",
-    Caption: "Electrify your friends!",
-    Items: [
-      {
-        Name: "Macbook Pro",
-        Image: "https://via.placeholder.com/640x360/fff",
-      },
-    ],
-  },
-  {
-    Name: "Two",
-    Caption: "Say no to manual home labour!",
-    Items: [
-      {
-        Name: "Washing Machine WX9102",
-        Image: "https://via.placeholder.com/640x360/aaa",
-      },
-    ],
-  },
-  {
-    Name: "Three",
-    Caption: "Give style and color to your living room!",
-    Items: [
-      {
-        Name: "Living Room Lamp",
-        Image: "https://via.placeholder.com/640x360/000",
-      },
-    ],
-  },
-  {
-    Name: "Four",
-    Caption: "Electrify your friends!",
-    Items: [
-      {
-        Name: "Macbook Pro",
-        Image: "https://via.placeholder.com/640x360/",
-      },
-    ],
-  },
-];
+    {
+        Name: "One",
+        Caption: "Electrify your friends!",
+        Items: [
+            {
+                Name: "Macbook Pro",
+                Image: "https://via.placeholder.com/640x360/fff"
+            }
+        ]
+    },
+    {
+        Name: "Two",
+        Caption: "Say no to manual home labour!",
+        Items: [
+            {
+                Name: "Washing Machine WX9102",
+                Image: "https://via.placeholder.com/640x360/aaa"
+            }
+        ]
+    },
+    {
+        Name: "Three",
+        Caption: "Give style and color to your living room!",
+        Items: [
+            {
+                Name: "Living Room Lamp",
+                Image: "https://via.placeholder.com/640x360/000"
+            }
+        ]
+    },
+    {
+        Name: "Four",
+        Caption: "Electrify your friends!",
+        Items: [
+            {
+                Name: "Macbook Pro",
+                Image: "https://via.placeholder.com/640x360/"
+            }
+        ]
+    }
+]
 
 class BannerExample extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      autoPlay: true,
-      animation: "fade",
-      indicators: true,
-      timeout: 500,
-      navButtonsAlwaysVisible: false,
-      navButtonsAlwaysInvisible: false,
-      cycleNavigation: true,
-    };
+        this.state = {
+            autoPlay: true,
+            animation: "fade",
+            indicators: true,
+            timeout: 500,
+            navButtonsAlwaysVisible: false,
+            navButtonsAlwaysInvisible: false,
+            cycleNavigation: true
+        }
 
-    autoBind(this);
-  }
+        autoBind(this);
+    }
 
-  toggleAutoPlay() {
-    this.setState({
-      autoPlay: !this.state.autoPlay,
-    });
-  }
+    toggleAutoPlay() {
+        this.setState({
+            autoPlay: !this.state.autoPlay
+        })
+    }
 
-  toggleIndicators() {
-    this.setState({
-      indicators: !this.state.indicators,
-    });
-  }
+    toggleIndicators() {
+        this.setState({
+            indicators: !this.state.indicators
+        })
+    }
 
-  toggleNavButtonsAlwaysVisible() {
-    this.setState({
-      navButtonsAlwaysVisible: !this.state.navButtonsAlwaysVisible,
-    });
-  }
+    toggleNavButtonsAlwaysVisible() {
+        this.setState({
+            navButtonsAlwaysVisible: !this.state.navButtonsAlwaysVisible
+        })
+    }
 
-  toggleNavButtonsAlwaysInvisible() {
-    this.setState({
-      navButtonsAlwaysInvisible: !this.state.navButtonsAlwaysInvisible,
-    });
-  }
+    toggleNavButtonsAlwaysInvisible() {
+        this.setState({
+            navButtonsAlwaysInvisible: !this.state.navButtonsAlwaysInvisible
+        })
+    }
 
-  toggleCycleNavigation() {
-    this.setState({
-      cycleNavigation: !this.state.cycleNavigation,
-    });
-  }
+    toggleCycleNavigation() {
+        this.setState({
+            cycleNavigation: !this.state.cycleNavigation
+        })
+    }
 
-  changeAnimation(event) {
-    this.setState({
-      animation: event.target.value,
-    });
-  }
+    changeAnimation(event) {
+        this.setState({
+            animation: event.target.value
+        })
+    }
 
-  changeTimeout(event, value) {
-    this.setState({
-      timeout: value,
-    });
-  }
+    changeTimeout(event, value) {
+        this.setState({
+            timeout: value
+        })
+    }
 
-  render() {
-    return (
-      <div style={{ marginTop: "50px", color: "#494949" }}>
-        <Carousel
-          className="Example"
-          autoPlay={this.state.autoPlay}
-          animation={this.state.animation}
-          indicators={this.state.indicators}
-          timeout={this.state.timeout}
-          cycleNavigation={this.state.cycleNavigation}
-          navButtonsAlwaysVisible={this.state.navButtonsAlwaysVisible}
-          navButtonsAlwaysInvisible={this.state.navButtonsAlwaysInvisible}
-          next={(now, previous) =>
-            console.log(
-              `Next User Callback: Now displaying child${now}. Previously displayed child${previous}`
-            )
-          }
-          prev={(now, previous) =>
-            console.log(
-              `Prev User Callback: Now displaying child${now}. Previously displayed child${previous}`
-            )
-          }
-          onChange={(now, previous) =>
-            console.log(
-              `OnChange User Callback: Now displaying child${now}. Previously displayed child${previous}`
-            )
-          }
-          // fullHeightHover={false}
-          // navButtonsProps={{style: {backgroundColor: 'cornflowerblue', borderRadius: 0}}}
-          // navButtonsWrapperProps={{style: {bottom: '0', top: 'unset', }}}
-          // indicatorContainerProps={{style: {margin: "20px"}}}
-          // NextIcon='next'
-        >
-          {items.map((item, index) => {
-            return (
-              <Banner
-                item={item}
-                key={index}
-                contentPosition={item.contentPosition}
-              />
-            );
-          })}
-        </Carousel>
+    render() {
+        return (
+            <div style={{ marginTop: "50px", color: "#494949" }}>
+                <Carousel
+                    className="Example"
+                    autoPlay={this.state.autoPlay}
+                    animation={this.state.animation}
+                    indicators={this.state.indicators}
+                    timeout={this.state.timeout}
+                    cycleNavigation={this.state.cycleNavigation}
+                    navButtonsAlwaysVisible={this.state.navButtonsAlwaysVisible}
+                    navButtonsAlwaysInvisible={this.state.navButtonsAlwaysInvisible}
+                    next={(now, previous) => console.log(`Next User Callback: Now displaying child${now}. Previously displayed child${previous}`)}
+                    prev={(now, previous) => console.log(`Prev User Callback: Now displaying child${now}. Previously displayed child${previous}`)}
+                    onChange={(now, previous) => console.log(`OnChange User Callback: Now displaying child${now}. Previously displayed child${previous}`)}
+                    // fullHeightHover={false}
+                    // navButtonsProps={{style: {backgroundColor: 'cornflowerblue', borderRadius: 0}}}
+                    // navButtonsWrapperProps={{style: {bottom: '0', top: 'unset', }}}
+                    // indicatorContainerProps={{style: {margin: "20px"}}}
+                    // NextIcon='next'
+                >
+                    {
+                        items.map((item, index) => {
+                            return <Banner item={item} key={index} contentPosition={item.contentPosition} />
+                        })
+                    }
+                </Carousel>
 
-        {/*                 <FormLabel component="legend">Options</FormLabel>
+
+{/*                 <FormLabel component="legend">Options</FormLabel>
                 <FormControlLabel
                     control={
                         <Checkbox onChange={this.toggleAutoPlay} checked={this.state.autoPlay} value="autoplay"
@@ -267,9 +258,10 @@ class BannerExample extends React.Component {
                         </div>
                     }
                 /> */}
-      </div>
-    );
-  }
+            </div>
+
+        )
+    }
 }
 
 export default BannerExample;
--- front/src/components/studio/index.js
+++ front/src/components/studio/index.js
@@ -1,22 +1,26 @@
-import React, { useEffect, Component } from "react";
-import { ReactFlvPlayer } from "react-flv-player";
+import React, { useEffect, Component } from 'react'
+import { ReactFlvPlayer } from 'react-flv-player'
 import Chat from "../chat";
-import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import Avatar from "@material-ui/core/Avatar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import { Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { Form } from 'react-bootstrap';
+import {
+  Link
+} from "react-router-dom";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    "& .MuiTextField-root": {
+    '& .MuiTextField-root': {
       margin: theme.spacing(1),
-      width: "100%",
+      width: '100%',
     },
   },
   large: {
@@ -29,75 +33,71 @@ const useStyles = makeStyles((theme) => ({
     textDecoration: "none",
   },
   paper: {
-    position: "absolute",
+    position: 'absolute',
     width: 400,
     backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
+    border: '2px solid #000',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
   root: {
-    width: "100%",
+    width: '100%',
     backgroundColor: theme.palette.background.paper,
   },
 }));
 
-var loc = window.location;
-var locToString = loc.toString();
-var id = locToString.substring(locToString.length - 1, locToString.length);
-var url = "" + id; // affichage des profil par id
+var loc = window.location
+var locToString = loc.toString()
+var id = locToString.substring(locToString.length-1, locToString.length)
+var url = '' + id; // affichage des profil par id
 
 class CardUser extends Component {
+
   state = {
-    user: {},
-  };
+     user : {}
+ }
 
-  componentDidMount() {
-    /* console.log(url) */
-    fetch("https://jsonplaceholder.typicode.com/users/1")
-      .then((response) => {
-        return response.json();
-      })
-      .then((result) => {
-        this.setState({ user: result });
-        console.log(result);
-      });
-  }
+ componentDidMount() {
+     /* console.log(url) */
+     fetch('https://jsonplaceholder.typicode.com/users/1')
+     .then(response => {
+         return response.json()
+     }).then(result => {
+         this.setState({user : result})
+         console.log(result)
+     })
+ }
 
-  render() {
-    return (
+ render() {
+    return(
       <div className="row test">
-        <Link
-          to="/profile"
-          style={{
-            width: "250px",
-            height: "250px",
-            textDecoration: "none",
-          }}
+        <Link to="/profile" style={{    
+          width: '250px',
+          height: '250px',
+          textDecoration: "none" }}
         >
-          <Avatar
-            alt={this.state.user.name}
-            src="/static/images/avatar/1.jpg"
-            style={{
-              width: "200px",
-              height: "200px",
-            }}
+          <Avatar alt={this.state.user.name} src="/static/images/avatar/1.jpg" style={{    
+            width: '200px',
+            height: '200px' }} 
           />
         </Link>
         <div className="NameStreamer">
-          <h2>{this.state.user.name}</h2>
+          <h2>
+            {this.state.user.name}
+          </h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
           </p>
           <table id="table">
             <thead>
-              <th>Followers</th>
-              <th>Followings</th>
+              <th>
+                Followers
+              </th>
+              <th>
+                Followings
+              </th>
             </thead>
             <tbody>
               <tr>
@@ -116,144 +116,168 @@ class CardUser extends Component {
           </table>
         </div>
         <div className="buttonDiv">
-          <Button
-            variant="contained"
-            className="input-item marginTop"
-            color="primary"
-          >
+          <Button variant="contained" className="input-item marginTop" color="primary">
             Suivre
           </Button>
         </div>
       </div>
-    );
+    )
+  }
+}
+
+class FormsChat extends React.Component{
+
+  constructor (props) {
+    super(props)
+      this.state = {
+          message:''
+      }
+      this.handleChange = this.handleChange.bind(this)
+      this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+      this.setState({
+          [e.target.name] : e.target.value,
+      })
+  }
+
+  handleSubmit(e) {
+      e.preventDefault();   
+      const {message} = this.state
+
+      const data = {message : message }
+          axios.post('http://localhost:1337/signin', data)
+          .then(response => {
+              this.setState({
+                  message: ''
+              })
+              console.log(response)
+          })
+          .catch(error => {
+              console.log(error)
+          })    
+  }
+
+  render() {
+    const { message } = this.state;
+    return(
+      <div className="input-group">
+        <form style={{ width: '100%' }}>
+          <Form.Group>
+            <Form.Control as="textarea" placeholder="tape your message" rows={2} id='message' name='message' value={message} onChange={this.handleChange} />
+          </Form.Group>
+          <Button type="submit" variant="contained" className="input-item marginTop" color="primary">
+            Envoyer 
+          </Button>
+        </form>
+      </div>
+    )
   }
 }
 
 function Studio(props) {
-  const testUrl = `${process.env.REACT_APP_BACK_ORIGIN}:${process.env.REACT_APP_STREAM_PORT}/live/TESTO.flv`;
+
 
   useEffect(() => {
     if (props.socket) {
-      const testChatroomId = "60d99924da81285294d066eb";
-      props.socket.emit("join room", testChatroomId);
+      const testChatroomId = '60d99924da81285294d066eb'
+      props.socket.emit('join room', testChatroomId)
       return () => {
-        props.socket.emit("leave room", testChatroomId);
-      };
+        props.socket.emit('leave room', testChatroomId)
+      }
     }
-  }, [props.socket]);
+  }, [props.socket])
 
   const classes = useStyles();
 
-  function Display(event) {
-    if (
-      event.target.parentNode.parentNode.childNodes[1].attributes[0].value ==
-      "false"
-    ) {
-      event.target.parentNode.parentNode.childNodes[1].attributes[0].value =
-        "true";
-    } else {
-      event.target.parentNode.parentNode.childNodes[1].attributes[0].value =
-        "false";
+    function Display(event) {
+      console.log(event.target.parentNode.parentNode.childNodes[1].attributes[0].value)
+      if (event.target.parentNode.parentNode.childNodes[1].attributes[0].value == "false") {
+        event.target.parentNode.parentNode.childNodes[1].attributes[0].value = 'true'
+      } else {
+        event.target.parentNode.parentNode.childNodes[1].attributes[0].value = 'false'
+      }
     }
-  }
 
-  function DisplayMode(event) {
-    var elmt = document.getElementsByClassName("containVideo");
-    if (event.target.innerText == "Mode Full Window") {
-      elmt[0].attributes[0].value = "false";
-      elmt[1].attributes[0].value = "true";
-    } else {
-      elmt[0].attributes[0].value = "true";
-      elmt[1].attributes[0].value = "false";
+    function DisplayMode(event) {
+      var elmt = document.getElementsByClassName('containVideo')
+      if (event.target.innerText == "Mode Full Window") {
+        elmt[0].attributes[0].value = 'false'
+        elmt[1].attributes[0].value = 'true'
+      } else {
+        elmt[0].attributes[0].value = 'true'
+        elmt[1].attributes[0].value = 'false'
+      }
     }
-  }
 
-  const playertoobar = (
-    <div className="playertoolbar">
-      <Button
-        onClick={Display}
-        variant="contained"
-        className="input-item marginTop"
-        color="default"
-      >
-        <FontAwesomeIcon icon={faEllipsisV} />
-      </Button>
-      <div aria-haspopup="false" className="window">
-        <List component="nav" className={classes.root} aria-label="contacts">
-          <ListItem button onClick={DisplayMode}>
-            <ListItemText inset primary="Mode Full Window" />
-          </ListItem>
-          <ListItem button onClick={DisplayMode}>
-            <ListItemText inset primary="Mode Studio" />
-          </ListItem>
-          <ListItem button>
-            <ListItemText inset primary="paramètres du stream" />
-          </ListItem>
-        </List>
-      </div>
-    </div>
-  );
+    const playertoobar =  <div className="playertoolbar">
+                            <Button onClick={Display} variant="contained" className="input-item marginTop" color="default">
+                              <FontAwesomeIcon icon={faEllipsisV}/>
+                            </Button>
+                            <div aria-haspopup="false" className="window">
+                            <List component="nav" className={classes.root} aria-label="contacts">
+                              <ListItem button onClick={DisplayMode}>
+                                <ListItemText inset primary="Mode Full Window" />
+                              </ListItem>
+                              <ListItem button onClick={DisplayMode}>
+                                <ListItemText inset primary="Mode Studio" />
+                              </ListItem>
+                              <ListItem button >
+                                <ListItemText inset primary="paramètres du stream" />
+                              </ListItem>
+                            </List>
+                            </div>
+                          </div>
 
   return (
-    <section>
-      <div aria-haspopup="true" className="container xl containVideo">
-        <div className="row row--With--Shadow">
-          <div className="col-sm-9">
-            {playertoobar}
-            <ReactFlvPlayer
-              url={testUrl}
-              height="auto"
-              width="100%"
-              isMuted={false}
-            />
+      <section>
+        <div aria-haspopup="true" className="container xl containVideo">
+          <div className="row row--With--Shadow">
+              <div className="col-sm-9">
+                      {playertoobar}
+                  <ReactFlvPlayer
+                      url="https://www.youtube.com/watch?v=GF04QkRU4es"
+                      height="auto"
+                      width="100%"
+                      isMuted={false}
+                  /> 
+              </div>
+              <div id="testCt" className="col-sm-3">
+                  <header className="titleChat">
+                      Chat du stream
+                  </header>
+                  <div className="containChat">
+                      <Chat/>
+                  </div>
+                  <FormsChat/>
+              </div>
           </div>
-          <div id="testCt" className="col-sm-3">
-            <header className="titleChat">Chat du stream</header>
-            <div className="containChat">
-              <Chat />
-            </div>
-            <div className="input-group">
-              <form style={{ width: "100%" }}>
-                <Form.Group>
-                  <Form.Control
-                    as="textarea"
-                    placeholder="tape your message"
-                    rows={2}
-                  />
-                </Form.Group>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  className="input-item marginTop"
-                  color="primary"
-                >
-                  Envoyer
-                </Button>
-              </form>
-            </div>
+          <br/>
+          <div className="row">
+              <div className="col-12">
+                <h2>
+                  Nom du stream
+                </h2>      
+              </div>
           </div>
+          <br/>
+          <CardUser/> 
         </div>
-        <br />
-        <div className="row">
-          <div className="col-12">
-            <h2>Nom du stream</h2>
-          </div>
-        </div>
-        <br />
-        <CardUser />
-      </div>
-      <div aria-haspopup="false" className="containFullMode containVideo">
+        <div aria-haspopup="false" className="containFullMode containVideo">
         {playertoobar}
-        <ReactFlvPlayer
-          url={testUrl}
-          height="auto"
-          width="100%"
-          isMuted={false}
-        />
-        <div className="containFullChat">{/* <Chat/> */}</div>
-      </div>
-    </section>
-  );
+          <ReactFlvPlayer
+            url="https://www.youtube.com/watch?v=GF04QkRU4es"
+            height="auto"
+            width="100%"
+            isMuted={false}
+          />
+          <div className="containFullChat">
+             <Chat/> 
+          </div>
+        </div>
+      </section>
+  )
 }
 
-export default Studio;
+export default Studio
