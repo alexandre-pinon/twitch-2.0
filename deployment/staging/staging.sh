b4732a824e23e3bc28dbeafdba05ce8b3ed773cd
Wbebey <wilfried@armis.tech>
2021-08-28 20:43:42 +0200

    feature(jenkins): add docker build

--- deployment/staging/staging.sh
+++ deployment/staging/staging.sh
@@ -10,15 +10,15 @@ step() {
 
 install_docker() {
     step "===== Installing docker ====="
-    sudo apt update
-    sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common gnupg lsb-release
+    sudo apt-get update
+    sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common gnupg lsb-release
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     if [ $? -ne 0 ]; then
         sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     fi
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    sudo apt update
-    sudo apt install -y docker-ce docker-ce-cli containerd.io
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
     # sudo groupadd docker
     sudo gpasswd -a $USER docker
     sudo chmod 777 /var/run/docker.sock
@@ -34,15 +34,30 @@ install_docker() {
 
 install_openssh() {
     step "===== Installing openssh ====="
-    sudo apt update
-    sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-    sudo apt install -y openssh-server
+    sudo apt-get update
+    sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    sudo apt-get install -y openssh-server
     sudo systemctl enable ssh
     sleep 5
 }
 
 install_tools() {
-    sudo apt install -y python3-pip
+    sudo apt-get install -y python3-pip
+}
+
+install_java() {
+    step "===== Installing java ====="
+    sudo apt-get install -y openjdk-8-jdk
+    java -version
+}
+
+install_jenkins() {
+    step "===== Installing jenkins ====="
+    wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+    sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+    sudo apt-get update && sudo apt-get upgrade -y
+    sudo apt-get install -y jenkins
+    sudo usermod -aG docker jenkins 
 }
 
 setup_root_login() {
@@ -51,7 +66,7 @@ setup_root_login() {
 }
 
 setup_welcome_msg() {
-    sudo apt -y install cowsay
+    sudo apt-get -y install cowsay
     sudo echo -e "\necho \"Welcome to Willy Ubuntu Server 20.04\" | cowsay\n" >> /home/vagrant/.bashrc
     sudo ln -s /usr/games/cowsay /usr/local/bin/cowsay
 }
@@ -60,6 +75,8 @@ main() {
     install_docker
     install_openssh
     install_tools
+    install_java
+    install_jenkins
     setup_root_login
     setup_welcome_msg
 }
