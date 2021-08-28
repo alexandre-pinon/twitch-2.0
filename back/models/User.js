6b23b1b9645fb1fa301f0141b1f9ee64d8ca0191
Alexandre Pinon <alexandre.pinon@epitech.eu>
2021-08-28 17:33:22 +0200

    feat: added 2 fields to user model

--- back/models/User.js
+++ back/models/User.js
@@ -42,7 +42,15 @@ const userSchema = new mongoose.Schema({
     type: [mongoose.Schema.Types.ObjectId],
     ref: 'User',
   },
+  followings: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'User',
+  },
   subscribers: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'User',
+  },
+  subscribings: {
     type: [mongoose.Schema.Types.ObjectId],
     ref: 'User',
   },
