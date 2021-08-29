5d8cc88059b8ac62214ae3d0ce2994bf7f363089
Alexandre Pinon <alexandre.pinon@epitech.eu>
2021-08-29 22:57:56 +0200

    feat: added logout

--- front/src/components/header/index.js
+++ front/src/components/header/index.js
@@ -1,78 +1,78 @@
-import React from "react";
-import clsx from "clsx";
-import { makeStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import Button from "@material-ui/core/Button";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Logo from "../images/icon.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faCog, faVideo } from "@fortawesome/free-solid-svg-icons";
-import Avatar from "@material-ui/core/Avatar";
+import React from 'react'
+import clsx from 'clsx'
+import { makeStyles } from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import Button from '@material-ui/core/Button'
+import List from '@material-ui/core/List'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import Logo from '../images/icon.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faCog, faVideo } from '@fortawesome/free-solid-svg-icons'
+import Avatar from '@material-ui/core/Avatar'
 
 const useStyles = makeStyles({
   list: {
     width: 250,
   },
   fullList: {
-    width: "auto",
+    width: 'auto',
   },
   menu: {
-    height: "4vh",
+    height: '4vh',
   },
   logo: {
-    width: "4vw",
-    height: "auto",
-    marginTop: "-39px",
+    width: '4vw',
+    height: 'auto',
+    marginTop: '-39px',
   },
   button: {
-    marginLeft: "-90%",
+    marginLeft: '-90%',
   },
   listItem: {
-    borderRadius: "50px",
+    borderRadius: '50px',
   },
-});
+})
 
 export default function TemporaryDrawer() {
-  const classes = useStyles();
+  const classes = useStyles()
   const [state, setState] = React.useState({
     left: false,
-  });
+  })
 
   const toggleDrawer = (anchor, open) => (event) => {
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return
     }
 
-    setState({ ...state, [anchor]: open });
-  };
+    setState({ ...state, [anchor]: open })
+  }
+
+  const logout = () => {
+    sessionStorage.removeItem('TOKEN')
+    window.location = '/login'
+  }
 
   const list = (anchor) => (
     <div
       className={clsx(classes.list, {
-        [classes.fullList]: anchor === "top" || anchor === "bottom",
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
       })}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-        {["Gotaga", "Sardoche", "Kameto", "Julia Bayonneta"].map(
-          (text, index) => (
-            <ListItem className={classes.listItem} button key={text}>
-              <ListItemIcon>
-                <Avatar>{text.substr(0, 1)}</Avatar>
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          )
-        )}
+        {['Gotaga', 'Sardoche', 'Kameto', 'Julia Bayonneta'].map((text, index) => (
+          <ListItem className={classes.listItem} button key={text}>
+            <ListItemIcon>
+              <Avatar>{text.substr(0, 1)}</Avatar>
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
       </List>
       <Divider />
       <List>
@@ -88,7 +88,7 @@ export default function TemporaryDrawer() {
           </ListItemIcon>
           <ListItemText primary="Réglages" />
         </ListItem>
-        <ListItem className={classes.listItem} button key="">
+        <ListItem className={classes.listItem} button key="" onClick={logout}>
           <ListItemIcon>
             <FontAwesomeIcon icon={faBars} />
           </ListItemIcon>
@@ -96,30 +96,23 @@ export default function TemporaryDrawer() {
         </ListItem>
       </List>
     </div>
-  );
+  )
 
   return (
     <div className={classes.menu}>
-      {["left"].map((anchor) => (
+      {['left'].map((anchor) => (
         <React.Fragment key={anchor}>
-          <Button
-            className={classes.button}
-            onClick={toggleDrawer(anchor, true)}
-          >
+          <Button className={classes.button} onClick={toggleDrawer(anchor, true)}>
             <FontAwesomeIcon icon={faBars} />
           </Button>
           <div>
             <img className={classes.logo} src={Logo} />
           </div>
-          <Drawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-          >
+          <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
             {list(anchor)}
           </Drawer>
         </React.Fragment>
       ))}
     </div>
-  );
+  )
 }
