e21dd67412673594f2c04f300a30f3087c602c8a
Wbebey <wilfried@armis.tech>
2021-08-30 17:56:54 +0200

    feature(kubernetes): add pipeline

--- deployment/staging/staging.sh
+++ deployment/staging/staging.sh
@@ -47,7 +47,7 @@ install_tools() {
 
 install_java() {
     step "===== Installing java ====="
-    sudo apt-get install -y openjdk-8-jdk
+    sudo apt-get install -y openjdk-11-jdk
     java -version
 }
 
@@ -58,6 +58,32 @@ install_jenkins() {
     sudo apt-get update && sudo apt-get upgrade -y
     sudo apt-get install -y jenkins
     sudo usermod -aG docker jenkins 
+}
+
+install_virtualbox() {
+    step "===== Installing virtualbox ====="
+    wget -q -O- http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | sudo apt-key add -
+    echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list 
+    sudo apt-get update
+    sudo apt-get install virtualbox-6.1 -y
+    sudo usermod -G vboxusers -a $USER
+
+}
+
+install_kubctl() {
+    step "===== Installing kubctl ====="
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+    chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
+    kubectl version --client
+}
+
+install_minikube() {
+    step "===== Installing minikube ====="
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube
+    sudo apt-get install -y conntrack
+    minikube start --driver=none
 }
 
 setup_root_login() {
@@ -75,8 +101,11 @@ main() {
     install_docker
     install_openssh
     install_tools
-    install_java
-    install_jenkins
+    # install_java
+    # install_jenkins
+    install_virtualbox
+    install_kubctl
+    install_minikube
     setup_root_login
     setup_welcome_msg
 }
